23e655cbfbbe0ca79e363112752c182fcdca8a4c
zbenii <besic.beni@gmx.at>
2018-09-18 11:29:13 +0200

    addded description

--- hello_world.c
+++ hello_world.c
@@ -6,11 +6,7 @@
  * Author:			<Benjamin Besic>
  * ----------------------------------------------------------
  * Description:
- * <some brief description, what the program does: example:
- * Calculates a pyramid of numbers, i.e., it multiplies a big
- * integer by the number 2, its result by 3, etc. and then
- * returns to the original value by subsequently dividing,
- * again starting by 2, 3, etc.>
+ * <You write a programm that says "Hello *your name*" in the Terminal>
  * ----------------------------------------------------------
  */
 
