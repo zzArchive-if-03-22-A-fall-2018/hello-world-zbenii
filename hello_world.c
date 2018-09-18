428b8ed858f7d5f6d7250492744ae871a6283370
zbenii <besic.beni@gmx.at>
2018-09-18 11:23:18 +0200

    wrote programm "Hello World" made screenshot of terminal

--- hello_world.c
+++ hello_world.c
@@ -1,6 +1,22 @@
+/*----------------------------------------------------------
+ *				HTBLA-Leonding / Class: <2AHIF>
+ * ---------------------------------------------------------
+ * Exercise Number: 1
+ * Title:			<Hello World>
+ * Author:			<Benjamin Besic>
+ * ----------------------------------------------------------
+ * Description:
+ * <some brief description, what the program does: example:
+ * Calculates a pyramid of numbers, i.e., it multiplies a big
+ * integer by the number 2, its result by 3, etc. and then
+ * returns to the original value by subsequently dividing,
+ * again starting by 2, 3, etc.>
+ * ----------------------------------------------------------
+ */
+
 #include <stdio.h>
 
 int main(int argc, char const *argv[]) {
-  printf("Hello world\n");
+  printf("Hello Benjamin Besic\n");
   return 0;
 }
